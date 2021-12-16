aecd49d4125555a1b75088d366097bd3dff19315
renren0530 <zoo.zoo.ren@gmail.com>
2021-12-16 16:57:26 +0900

    修正24

--- app/models/brand_kinds.rb
+++ app/models/brand_kinds.rb
@@ -1,6 +1,8 @@
 class BrandKinds < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
-    { id: 5, name: 'シュライヒ' }, { id: 15, name: 'リーデル' }, { id: 25, name: 'ナハトマン' }
+    { id: 5, name: 'シュライヒ' }, { id: 15, name: 'リーデル' }, { id: 25, name: 'ナハトマン' },
+    { id: 35, name: 'カリマー' }, { id: 45, name: 'ペニー' }, { id: 55, name: 'sigg' },
+    { id: 65, name: 'ペトロマックス' }
   ]
 end
