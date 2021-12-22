e549a5f67dac7ab965a28b691bfcce3042a7b075
renren0530 <zoo.zoo.ren@gmail.com>
2021-12-22 10:58:20 +0900

    修正29

--- app/javascript/shiaeru-image.js
+++ app/javascript/shiaeru-image.js
@@ -18,7 +18,12 @@ for(var i = 0; i < imgList.length; i++) {
 	// li要素をクラス名「slider-inner」の子要素として追加
 	document.getElementsByClassName("slider-inner")[0].appendChild(slide);
 
-
+	// li要素を取得
+	var nav = document.createElement("li");
+	// プロパティ「data-nav-index」に数値を割り振る
+	nav.setAttribute("data-nav-index", i);
+	// li要素をクラス名「nav」の子要素として追加
+	document.getElementsByClassName("nav")[0].appendChild(nav);
 }
 
 // スライドの数を取得(処理のために-1する)
@@ -27,15 +32,21 @@ var length = imgList.length - 1;
 // クラス名「imageSlide」に画像の1枚の要素を格納
 var imageSlide = document.getElementsByClassName("slider-inner")[0].getElementsByTagName("li");
 
+// クラス名「dotNavigation」にドットナビの1つの要素を格納
+var dotNavigation = document.getElementsByClassName("nav")[0].getElementsByTagName("li");
+
 // 「現在○○枚目のスライドを表示している」というインデックス番号を格納する変数
 var nowIndex = 0;
 // 現在表示されている画像とドットナビにクラス名を付ける
 imageSlide[nowIndex].classList.add("show");
+dotNavigation[nowIndex].classList.add("current");
 
 // スライドがアニメーション中か判断するフラグ
 var isChanging = false;
+
 // スライドのsetTimeoutを管理するタイマー
 var slideTimer;
+
 // スライド切り替え時に呼び出す関数
 function sliderSlide(val) {
 	if (isChanging === true) {
@@ -44,9 +55,13 @@ function sliderSlide(val) {
 	isChanging = true;
 	// 現在表示している画像とナビからクラス名を削除
 	imageSlide[nowIndex].classList.remove("show");
+	dotNavigation[nowIndex].classList.remove("current");
 	nowIndex = val;
+
 	// 次に表示するスライドとナビにカレントクラスを設定
 	imageSlide[nowIndex].classList.add("show");
+	dotNavigation[nowIndex].classList.add("current");
+
 	// アニメーションが終わるタイミングでisChangingのステータスをfalseに
 	slideTimer = setTimeout(function(){
 		isChanging = false;
@@ -70,6 +85,15 @@ document.getElementById("arrow-next").addEventListener("click", function(){
 	}
   sliderSlide(index);
 }, false);
+
+// ドットナビをクリックした時のイベントを作成
+for(var i = 0; i < dotNavigation.length; i++) {
+	// データ属性のインデックス番号を元にスライドを行う
+	dotNavigation[i].addEventListener("click", function(){
+		var index = Number(this.getAttribute("data-nav-index"));
+		sliderSlide(index);
+	}, false);
+}
 
 })
 
