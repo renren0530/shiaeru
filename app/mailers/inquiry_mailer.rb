b594d7c712ff3f1296d689aafa1a91df5d043f1d
renren0530 <zoo.zoo.ren@gmail.com>
2021-12-23 15:45:25 +0900

    修正38

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,6 +1,6 @@
 class InquiryMailer < ApplicationMailer
   def send_mail(inquiry)
     @inquiry = inquiry
-    mail to: ENV['GOOGLE_MAIL_ADDRESS'], subject: '【お問い合わせ】'
+    mail to: "info@shiaeru.net", subject: '【お問い合わせ】'
   end
 end
