186c72934fcf18aaf0ab0ce632b563b9cb28ab11
renren0530 <zoo.zoo.ren@gmail.com>
2021-12-23 15:16:13 +0900

    修正37

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,7 +2,6 @@ class WelcomeMailer < ApplicationMailer
 
   def send_when_signup(email,nickname)
     @nickname = nickname
-    mail to: email, subject: 'しあえる 登録完了のお知らせ',
-    from: '"しあえる" <noreply@yoursite.com>'
+    mail to: email, subject: 'しあえる 登録完了のお知らせ'
   end
 end
