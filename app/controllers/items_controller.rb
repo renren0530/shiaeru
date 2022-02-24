055668a30dd04bbec5db5ba420d6c6c2f76da924
renren0530 <zoo.zoo.ren@gmail.com>
2022-02-24 11:29:11 +0900

    修正70

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,9 @@ class ItemsController < ApplicationController
 
 
   def index
-    @items = Item.all.order('created_at ASC').limit(10)
-    @brands = Brand.all.order('created_at ASC').limit(10)
-    @returns = Return.all.order('created_at ASC').limit(10)
+    @items = Item.all.order('created_at ASC').limit(12)
+    @brands = Brand.all.order('created_at ASC').limit(12)
+    @returns = Return.all.order('created_at ASC').limit(12)
   end
 
   def new
