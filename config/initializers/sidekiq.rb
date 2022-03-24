e99adaffeb09bbaab2fe207a11cdd5902f7c786d
renren0530 <zoo.zoo.ren@gmail.com>
2022-03-24 14:31:19 +0900

    修正78

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'http://proxy:29ffa8b9ae0842c7b0a1398b4ed26f28@proxy-54-225-208-216.proximo.io' }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'http://proxy:29ffa8b9ae0842c7b0a1398b4ed26f28@proxy-54-225-208-216.proximo.io' }
+# end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'http://proxy:29ffa8b9ae0842c7b0a1398b4ed26f28@proxy-54-225-208-216.proximo.io' }
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'http://proxy:29ffa8b9ae0842c7b0a1398b4ed26f28@proxy-54-225-208-216.proximo.io' }
+# end
